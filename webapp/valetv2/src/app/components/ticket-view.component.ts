cf683d22e628cbf3fc55d91b9767a1e45f1a4b22
Maham Shahid <mahum.shahid934@gmail.com>
2018-08-26 22:55:27 +0500

    Implemented ticket view functionality

--- webapp/valetv2/src/app/components/ticket-view.component.ts
+++ webapp/valetv2/src/app/components/ticket-view.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { AuthService } from '../services/auth.service';
+import { DataService } from '../services/data.service';
+import { TokenUtilService } from '../services/token-util.service';
+
+import { switchMap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-ticket',
@@ -6,6 +13,53 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
     styleUrls: ['../../styles/components/ticket-view.component.scss']
 })
 
-export class TicketViewComponent {
+export class TicketViewComponent implements OnInit, OnDestroy {
+
+    user: any = {
+        first_name: '',
+        last_name: '',
+        car: {
+            reg_no: '',
+            color: '',
+            manufacturer: '',
+            model: ''
+        },
+        ticket: {
+            paid: '',
+            amount: '',
+            no: ''
+        }
+    };
+
+    constructor(private auth: AuthService,
+        private data: DataService,
+        private tokenUtil: TokenUtilService,
+        private route: ActivatedRoute,
+        private router: Router) {
+    }
+
+    ngOnInit(): void {
+        this.tokenUtil.isTokenValid()
+            .subscribe(() => {
+                this.getData();
+            });
+    }
+
+    ngOnDestroy(): void {
+    }
+
+    getData() {
+        let ticket_no = null;
+        this.route.params
+            .pipe(
+                switchMap(param => {
+                    ticket_no = param.ticket_no;
+                    return this.data.getUser(ticket_no, this.tokenUtil.getToken());
+                })
+            )
+            .subscribe((response) => {
+                this.user = response;
+            });
+    }
 
 }
--- webapp/valetv2/src/app/services/data.service.ts
+++ webapp/valetv2/src/app/services/data.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  url = '';
+
+  constructor(private http: HttpClient) {
+    this.url = environment.serverBaseUrl;
+  }
+
+  getAuthHeader(token) {
+    return {
+      headers: new HttpHeaders({
+        'authorization': token
+      })
+    };
+  }
+
+  getUser(ticketNo, token) {
+    return this.http.get(`${this.url}/user?ticket=${ticketNo}`,
+      this.getAuthHeader(token));
+  }
 }
